6796ce8f6b7b6ddf5b7f3d46b5e25a99aa83bb23
Matteo Bonfanti <mat.bonfanti@gmail.com>
2023-02-08 15:31:00 +0100

    fixing bug in extension check

--- resources/create_markdown.py
+++ resources/create_markdown.py
@@ -58,10 +58,10 @@ def main():
     print(INDEX_TEMPLATE_HEADER.format(config_dict["short_title"], config_dict["title"], config_dict["subtitle"],
                                        organization_name, repo_name))
     for k, v in config_dict["pages"].items():
-      if os.path.splitext(v["notebook"])[1] == "ipynb":
+      if os.path.splitext(v["notebook"])[1] == ".ipynb":
         ghpages_link = "https://{0}.github.io/{1}/{2}".format(organization_name, repo_name, v["notebook"].replace("ipynb", "html"))
         print(INDEX_TEMPLATE_LINKLINE.format(k, v["notebook"], ghpages_link, v["description"]))
-      elif os.path.splitext(v["notebook"])[1] == "html":
+      elif os.path.splitext(v["notebook"])[1] == ".html":
         ghpages_link = "https://{0}.github.io/{1}/{2}".format(organization_name, repo_name, v["notebook"])
         print(INDEX_TEMPLATE_LINKLINE_ONLY_HTML.format(k, ghpages_link, v["description"]))        
     print(INDEX_TEMPLATE_FINAL)
